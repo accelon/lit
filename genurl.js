c9fc8520b75a5af5d4935c89f9691f3bb4071b42
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2021-09-28 16:45:53 +0630

    add books

---
+++ genurl.js
@@ -0,0 +1,8 @@
+﻿import books from './books.js';
+const links=[];
+for (let i in books) {
+	const url='http://www.open-lit.com/html/GetFile.php?getType=lit&getId='+i;
+	//console.log('curl '+url+' > '+ i+'.zip');
+	console.log(url)
+}
+
